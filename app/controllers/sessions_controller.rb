e77bd801b88744295ee8d570804593c9a7f18cbd
John <john.haggerud@lnu.se>
2015-02-23 16:44:41 +0000

    Some minor update

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -38,14 +38,14 @@ class SessionsController < ApplicationController
  
   ## This is called from a client who wish to authenticate and get a JSON Web Token back
    def api_auth 
-     
-     user = User.find_by(email: params[:email].downcase)
-    if user && user.authenticate(params[:password])
-      
-      render json: { auth_token: encodeJWT(user) }
-    else
-      render json: { error: 'Invalid username or password' }, status: :unauthorized
-    end
+      # output the APIkey from the header
+      # puts request.headers["X-APIkey"]; 
+      user = User.find_by(email: params[:email].downcase)
+      if user && user.authenticate(params[:password])
+        render json: { auth_token: encodeJWT(user) }
+      else
+        render json: { error: 'Invalid username or password' }, status: :unauthorized
+      end
   end
   
 end
